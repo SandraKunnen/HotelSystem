52310cc6c3e4a03bff828f9f57847a4512c64db1
dwvvz <diederick.veldhuijzen.van.zanten@capgemini.com>
2017-05-03 14:19:28 +0200

    Class Person added

---
+++ src/main/java/com/capgemini/hotelapp/model/Boeking.java
@@ -0,0 +1,18 @@
+package com.capgemini.hotelapp.model;
+
+import java.time.LocalDateTime;
+import java.time.Period;
+
+/**
+ * Created by diveldhu on 20-4-2017.
+ */
+public interface Boeking {
+    public Boeking getBoeking(Boeking b);
+    public void setBoeking(Kamer k, Period p);
+    public Kamer getGeboekteKamer();
+    public void setGeboekteKamer(Kamer k);
+    public LocalDateTime getStartDatum();
+    public void setStartDatum(LocalDateTime l);
+    public LocalDateTime getEindDatum();
+    public void setEindDatum(LocalDateTime l);
+}
---
+++ src/main/java/com/capgemini/hotelapp/model/Kamer.java
@@ -0,0 +1,11 @@
+package com.capgemini.hotelapp.model;
+
+/**
+ * Created by diveldhu on 20-4-2017.
+ */
+public interface Kamer {
+    public int getKamernummer();
+    public void setKamernummer(int i);
+    public boolean isLuxeKamer();
+    public void setLuxeKamer(boolean b);
+}
