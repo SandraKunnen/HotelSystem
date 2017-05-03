2971d79e110fe486be1a83f80814247bb5c37efa
dwvvz <diederick.veldhuijzen.van.zanten@capgemini.com>
2017-05-03 11:28:22 +0200

    Class Person added

--- src/main/java/com/capgemini/hotelapp/model/Person.java
+++ src/main/java/com/capgemini/hotelapp/model/Person.java
@@ -7,11 +7,6 @@ public class Person {
     private String name;
     private int age;
 
-    Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
     public int getAge() {
         return age;
     }
